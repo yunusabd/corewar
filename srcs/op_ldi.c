bb2c8ca7612eac7d428f0d1a075d97c0f019a94b
yunusabd <yunusemin-github@yahoo.com>
2018-08-10 16:36:03 +0200

    fixed bug in get_size

--- srcs/op_ldi.c
+++ srcs/op_ldi.c
@@ -1,35 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_size.c                                         :+:      :+:    :+:   */
+/*   op_ldi.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yabdulha <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/07/29 00:00:33 by yabdulha          #+#    #+#             */
-/*   Updated: 2018/08/10 16:31:43 by yabdulha         ###   ########.fr       */
+/*   Created: 2018/08/10 15:59:15 by yabdulha          #+#    #+#             */
+/*   Updated: 2018/08/10 16:10:06 by yabdulha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
 
-void	get_size(t_vm *vm, t_champ *champ)
+void	op_ldi(t_vm *vm, t_champ *champ)
 {
-	int		res;
-	int		i;
-	t_byte	*tmp;
-
-	res = 0;
-	i = 0;
-	tmp = champ->bytes;
-	while (tmp && i++ < PROG_NAME_LENGTH + 8)
-		tmp = tmp->next;
-	i = 0;
-	while (tmp && i++ < 4)
-	{
-		res = res << 8;
-		res |= (unsigned char)tmp->byte;
-		tmp = tmp->next;
-	}
-	champ->size = res;
-	printf("\n\nSIZE: %d\n", res);
+	get_parameters(vm, champ);
+	resolve_params(vm, champ, champ->params, 3);
 }
